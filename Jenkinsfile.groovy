1eb1c80c9bbc163d138f30378c941a54ea5a0184
neshimbekova <neshimbekova@gmail.com>
2019-05-04 15:36:29 -0500

    fgdh

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,15 @@
+node {
+  properties([parameters([string(defaultValue: 'IP', description: 'Where to build?', name: 'ENV', trim: true)])])
+  stage("Clone repo"){
+    git 'git@github.com:neshimbekova/stormpath.git'
+  }
+  stage('Build application') {
+    sh "ssh ec2-user@${ENV} sudo yum install python-pip -y"
+    sh "scp -r * ec2-user@${ENV}:/tmp"
+    sh "ssh ec2-user@${ENV} sudo pip install -r /tmp/requirements.txt"
+
+  }
+  stage("App Run"){
+    sh "ssh ec2-user@${ENV} python /tmp/app.py"
+  }
+}
